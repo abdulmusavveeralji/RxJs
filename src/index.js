11e1a7efd93f13166c342d205a376afa3fb1c6b9
Abdul Musavveer Alji <abdul.m@ascentbusiness.com>
2023-06-18 16:11:14 +0530

    async Observable

--- src/index.js
+++ src/index.js
@@ -19,3 +19,24 @@ observable.subscribe({
         console.error(err);
     }
 })
+
+console.log('before')
+const asyncObservable = new Observable((subscriber) => {
+    setInterval(() => {
+        subscriber.next('Sufiyan')
+    }, 1000)
+});
+
+asyncObservable.subscribe({
+    next: (value) => {
+        console.log(value);
+    },
+    complete: () => {
+        console.log('Complete Called!')
+    },
+    error: (err) => {
+        console.error(err);
+    }
+})
+
+console.log('after')
