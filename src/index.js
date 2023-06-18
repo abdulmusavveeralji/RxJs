c2becc4791422db5fac39eb980e18e63e4d33255
Abdul Musavveer Alji <abdul.m@ascentbusiness.com>
2023-06-18 16:03:21 +0530

    Observable

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { Observable } from "rxjs";
+
+const observable = new Observable((subscriber) => {
+    subscriber.next('Hello Musavveer')
+    subscriber.error('Error!');
+    subscriber.next('Test')
+    subscriber.complete()
+    subscriber.next('Next')
+});
+
+observable.subscribe({
+    next: (value) => {
+        console.log(value);
+    },
+    complete: () => {
+        console.log('Complete Called!')
+    },
+    error: (err) => {
+        console.error(err);
+    }
+})
